fafc005e574f742cd3db34996f1488fa82f93915
4eburek-hab <piton4ik89@gmail.com>
2020-12-28 18:15:03 +0300

    Задание из практики

---
+++ Practical.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.lesson1.git;
+
+import java.util.Random;
+
+public class Praktika {
+    public static void main(String[] args) {
+
+        int a = 0;
+        int b =10;
+        int random_nambr1 = a + (int) (Math.random() * b);
+        System.out.println("11111" + random_nambr1);
+    }
+}
