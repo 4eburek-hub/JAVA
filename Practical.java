8e4281c6b66faa2d45e1a492efaa0621823c831f
4eburek-hab <piton4ik89@gmail.com>
2020-12-28 18:15:30 +0300

    Задание из практики

--- Practical.java
+++ Practical.java
@@ -1,13 +1,37 @@
 package ru.geekbrains.lesson1.git;
 
 import java.util.Random;
+import java.util.Scanner;
 
-public class Praktika {
+public class Practical {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
+        int try3 = 0;
+        int random = rand.nextInt(9);
+        System.out.println("Угадай число с трёх раз");
 
-        int a = 0;
-        int b =10;
-        int random_nambr1 = a + (int) (Math.random() * b);
-        System.out.println("11111" + random_nambr1);
-    }
+        while (try3 < 3){
+        System.out.println("Введите чесло от 0 до 9");
+        int namber = input.nextInt();
+        if (namber > random) {
+            System.out.println("Ваше число больше!: ");
+        } else if (namber < random) {
+            System.out.println("Ваше чило меньше!: ");
+        }
+        else if (namber == random) {
+            System.out.println("Ура! Вы отгадали");
+        }
+        try3++;
+        }
+        System.out.println("Ваши попытки закончились. Повторить игру еще раз? 1 – да / 0 – нет");
+        String userData = input.next();
+        if (userData.equals("1")) {
+            main(null);
+        }
+}
+    public static void Str(String[] args) {
+        String [] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+
+}
 }
